00be8f1efe576f26b38ee17fc185875fc9651d98
Marouen <marouen.b.guebila@gmail.com>
2018-11-05 22:40:48 +0100

    branching

--- code/analysis.r
+++ code/analysis.r
@@ -2,15 +2,50 @@
 library(DistMap)#for ground truth
 library(useful)#for corner
 library(nnet)#needed function to compute position of max
-#library(devtools)
-#install_github("marouenbg/seurat")
+library(devtools)
+install_github("marouenbg/seurat")
 #Install my version of seurat where I fixed a few things (l15 rbind in function inside FitGeneK)
 library(Seurat)#Loading my version of Seurat
 #require(scales)#for hue palette
 #library(XLConnect) #for reading xls
 
+accuracyTest <- function(posMat,geometry,nbt,convTable,raw.data,numberGenes,mapping,varMethod){
+  #final mapping and convert back to original coordinates
+  mapMat = matrix(0L, nrow = dim(nbt@spatial@final.prob)[1], ncol = dim(raw.data)[2])
+  for(i in 1:dim(raw.data)[2]){
+    mapMat[,i]=convTable[order(nbt@spatial@final.prob[,i]),1]#p values are from the smallest
+  }
+  
+  #remove 3040 vv
+  mapMat = mapMat[-3040,]
+  
+  #write accuracy test
+  #1. counting if max MCC cell is in top 10
+  count=0
+  for(i in 1:dim(posMat)[2]){
+    if (posMat[1,i] %in% mapMat[1:10,i]){
+      count=count+1
+    } 
+  }
+  perc=count/dim(posMat)[2]#percentage of cells identified corrcetly in top 10
+  #2. average distance between cells and the max MCC cell
+  avDistance=0
+  for(i in 1:dim(posMat)[2]){
+    avDistance=avDistance+dist(rbind(geometry[posMat[1,i],], geometry[mapMat[1,i],]))
+  }
+  avDistance=avDistance/dim(posMat)[2]
+  
+  #write results to csv file
+  write.csv(t(mapMat)[,1:10], paste("submission",numberGenes,mapping,varMethod,".csv",sep=""), row.names = 1:dim(raw.data)[2], sep=",")
+  return(list(perc,avDistance))
+}
+
+#Parameters
+mapping='projection'
+binary='seurat'
+
 #change working directory
-setwd("/home/marouen/dreamChallenge/data/singleCellData")
+setwd("/home/marouen/challenges/dreamChallenge/data/singleCellData")
 
 ##load data
 #Raw data
@@ -95,26 +130,33 @@ rownames(raw.data)[which(rownames(raw.data)=="Blimp-1")]="DebugBlimp1"
 rownames(raw.data)[which(rownames(raw.data)=="E(spl)m5-HLH")]="DebugEsplm5HLH"
 colnames(insitu.matrix)[which(colnames(insitu.matrix)=="Blimp-1")]="DebugBlimp1"
 colnames(insitu.matrix)[which(colnames(insitu.matrix)=="E(spl)m5-HLH")]="DebugEsplm5HLH"
-
+# and binary DistMap as well
+if (binary=='genDistmap'){
+  rownames(dm@binarized.data)[which(rownames(dm@binarized.data)=="Blimp-1")]="DebugBlimp1"
+  rownames(dm@binarized.data)[which(rownames(dm@binarized.data)=="E(spl)m5-HLH")]="DebugEsplm5HLH"
+}
+  
 #conversion table of points between the actual repersentation
 #and a projection compatible with seurat where the first cell
 #is in the top left corner
 convTable  = matrix(0L, nrow = dim(geometry)[1]+1, ncol = 2)
 convTable[,1]=1:3040 #first column is new system
-convTable[1:3039,2]=order(-geometry[,3],geometry[,1])#sort by decreasing 3, and increasing 1
-convTable[3040,2]=3040
+if(mapping=='original'){
+    convTable[,2]=1:3040
+}else if (mapping=='projection'){
+    convTable[1:3039,2]=order(-geometry[,3],geometry[,1])#sort by decreasing 3, and increasing 1
+    convTable[3040,2]=3040
+}
 
 #Call Seurat and some hacks to prevent errors
-#meta.data <- data.frame(rep(1, ncol(raw.data)))
-#ident = factor(rep(1, ncol(raw.data)))
 colnames(raw.data)=colnames(normalized.data)#Cell names
 nbt=CreateSeuratObject("seurat",raw.data=raw.data)
-#names(nbt@ident) = colnames(normalized.data)
 
 #Scale Data
 nbt=NormalizeData(nbt)
 nbt=ScaleData(nbt)
-nbt=FindVariableGenes(nbt,mean.function = ExpMean,dispersion.function = LogVMR)
+nbt=FindVariableGenes(nbt,mean.function = ExpMean,dispersion.function = LogVMR, y.cutoff=1, x.low.cutoff = 0.1,
+x.high.cutoff = 8)
 
 #PCA
 nbt=RunPCA(nbt, do.print=FALSE, pcs.compute = 40)#because there were more than 20 sig PCs, PCA on variable genes
@@ -129,78 +171,84 @@ sigPCs=24
 nbt <- ProjectPCA(nbt, do.print = FALSE,do.center=FALSE)
 genes.sig <- PCASigGenes(nbt,pcs.use = 1:sigPCs, pval.cut = 1e-2, use.full = FALSE)#this should be TRUE but documentation says it is ok
 
-#tSNE
-nbt=RunTSNE(nbt,dims.use = 1:sigPCs,max_iter=2000)
-TSNEPlot(nbt,pt.size = 1)
-
-#Clustering
-nbt=DBClustDimension(nbt,1,2,reduction.use = "tsne",G.use = 2.2,set.ident = TRUE)#DBScan
-#nbt=KClustDimension(nbt,1,2,reduction.use = "tsne",set.ident = TRUE, k.use = 10)#KNN
-nClusters=length(unique(factor(nbt@ident)))#1st cluster is un-assigned cells
-TSNEPlot(nbt,pt.size = 1)
-
-#Plot cells against each other
-par(mfrow=c(2,2))
-CellPlot(nbt,nbt@cell.names[1],nbt@cell.names[2],do.ident = FALSE)
-CellPlot(nbt,nbt@cell.names[3],nbt@cell.names[4],do.ident = FALSE)
-
-#Add in situ genes
-#I project the spheroid into rectangle
-#I added 1 empty location to make 3039+1=76*40 or 80*38 or 95*32 (Leading dimension is 76= ncols)
-insitu.matrix=rbind(insitu.matrix,integer(dim(insitu.matrix)[2]))
-insitu.genes=colnames(insitu.matrix)
-#reorder bins
-insitu.matrix=insitu.matrix[convTable[,2],]
-nbt@spatial@insitu.matrix =as.data.frame(insitu.matrix)
-lasso.genes.use=unique(c(genes.sig,nbt@var.genes))
-
-#Impute gene expression
-nbt <- AddImputedScore(nbt, genes.use=lasso.genes.use,genes.fit=insitu.genes, do.print=FALSE, s.use=40, gram=FALSE)
-
-#Continue with Seurat mapping
-#i="rho" i="Blimp-1"
-for(i in rev(insitu.genes)){
-  nbt=FitGeneK(nbt,i,do.plot=FALSE,do.k = 2,start.pct=mean(nbt@spatial@insitu.matrix[,i]),num.iter = 1)
-} 
-
-#show an example mixture model
-#par(mfrow=c(2,2))
-#nbt_temp=FitGeneK(nbt,"zfh1",do.plot=TRUE,do.k = 2,start.pct=mean(nbt@spatial@insitu.matrix[,"zfh1"]))
-#nbt_temp=FitGeneK(nbt,"zen2",do.plot=TRUE,do.k = 2,start.pct=mean(nbt@spatial@insitu.matrix[,"zen2"]))
-#nbt_temp=FitGeneK(nbt,"twi",do.plot=TRUE,do.k = 2,start.pct=mean(nbt@spatial@insitu.matrix[,"twi"]))
-#nbt_temp=FitGeneK(nbt,"tsh",do.plot=TRUE,do.k = 2,start.pct=mean(nbt@spatial@insitu.matrix[,"tsh"]))
-
-#Map cells
-nbt <- InitialMapping(nbt)
-
-#Refine mapping
-num.pc=3; num.genes=6
-genes.use=PCTopGenes(nbt,pc.use = 1:num.pc,num.genes = num.genes,use.full = TRUE,do.balanced = TRUE)
-
-#impute values for those genes
-new.imputed=genes.use[!genes.use%in%rownames(nbt@imputed)]
-lasso.genes.use=unique(c(nbt@var.genes,PCASigGenes(nbt,pcs.use = c(1,2,3), pval.cut = 1e-2, use.full = FALSE)))
-#use.full should be TRUE but it throws error, the documentation says FALSE is ok
-nbt <- AddImputedScore(nbt, genes.use=lasso.genes.use,genes.fit=new.imputed, do.print=FALSE, s.use=40, gram=FALSE)
-
-#Actual refinment
-nbt <- RefinedMapping(nbt,genes.use)
-
-#identify positions
-corner(nbt@spatial@final.prob)
-
-#calculate centroids for each cell (we do not need this step because here 1bin=1cell)
-nbt.centroids=GetCentroids(nbt); colnames(nbt.centroids)=c("bin.tier","bin.dv")
-corner(nbt.centroids)
-
-#final mapping and convert back to original coordinates
-mapMat = matrix(0L, nrow = dim(nbt@spatial@final.prob)[1], ncol = dim(raw.data)[2])
-for(i in 1:dim(raw.data)[2]){
-  mapMat[,i]=convTable[1,order(nbt@spatial@final.prob[,i])]#p values are from the smallest
+insitu.genes.save=insitu.genes
+insitu.matrix.save=insitu.matrix
+hvg.info.save=nbt@hvg.info
+for(numberGenes in c(20,40,60)){
+  for(varMethod in c("dispersion","geneMean")){
+    insitu.genes=insitu.genes.save
+    insitu.matrix=insitu.matrix.save
+    nbt@hvg.info=hvg.info.save
+    #select genes based on variability
+    #1-dispersion/ 2 gene expression mean
+    if(varMethod=='dispersion'){
+      #the dispersion is default order
+    }else if(varMethod=='geneMean'){
+      nbt@hvg.info=nbt@hvg.info[order(-nbt@hvg.info$gene.mean),]
+    }
+    #intersect and keep order of priority
+    insitu.genes=intersect(rownames(nbt@hvg.info),insitu.genes)
+    if(numberGenes==20){
+      insitu.genes=insitu.genes[1:20]
+    }else if(numberGenes==40){
+      insitu.genes=insitu.genes[1:40]
+    }else if(numberGenes==60){
+      insitu.genes=insitu.genes[1:60]
+    }
+    insitu.matrix=insitu.matrix[,insitu.genes]
+    #Add in situ genes
+    #I project the spheroid into rectangle
+    #I added 1 empty location to make 3039+1=76*40 or 80*38 or 95*32 (Leading dimension is 76= ncols)
+    insitu.matrix=rbind(insitu.matrix,integer(dim(insitu.matrix)[2]))
+    insitu.genes=colnames(insitu.matrix)
+    #reorder bins
+    insitu.matrix=insitu.matrix[convTable[,2],]
+    nbt@spatial@insitu.matrix =as.data.frame(insitu.matrix)
+    lasso.genes.use=unique(c(genes.sig,nbt@var.genes))
+    
+    #Impute gene expression
+    nbt <- AddImputedScore(nbt, genes.use=lasso.genes.use,genes.fit=insitu.genes, do.print=FALSE, s.use=40, gram=FALSE)
+    
+    #use binarization of DistMap instead of mixture models
+    if (binary=='genDistmap'){
+      dmBinary = t(dm@binarized.data)+1
+      write.table(dmBinary, file = "/home/marouen/seurat/R/dmBinary", sep = " ")
+    }
+    
+    #Continue with Seurat mapping
+    #i="rho" i="Blimp-1"
+    for(i in rev(insitu.genes)){
+      nbt=FitGeneK(nbt,i,do.plot=FALSE,do.k = 2,start.pct=mean(nbt@spatial@insitu.matrix[,i]),num.iter = 1)
+    } 
+    
+    #Map cells
+    mapping='Initial'
+    nbt <- InitialMapping(nbt)
+    resultsBeforeRefine=accuracyTest(posMat,geometry,nbt,convTable,raw.data,numberGenes,mapping,varMethod)
+    print(resultsBeforeRefine)
+    
+    #Refine mapping
+    num.pc=2; num.genes=4
+    genes.use=PCTopGenes(nbt,pc.use = 1:num.pc,num.genes = num.genes,use.full = TRUE,do.balanced = TRUE)
+    
+    #impute values for those genes
+    new.imputed=genes.use[!genes.use%in%rownames(nbt@imputed)]
+    lasso.genes.use=unique(c(nbt@var.genes,PCASigGenes(nbt,pcs.use = c(1,2,3), pval.cut = 1e-2, use.full = FALSE)))
+    #use.full should be TRUE but it throws error, the documentation says FALSE is ok
+    nbt <- AddImputedScore(nbt, genes.use=lasso.genes.use,genes.fit=new.imputed, do.print=FALSE, s.use=40, gram=FALSE)
+    
+    #Actual refinment
+    nbt <- RefinedMapping(nbt,genes.use)
+    
+    #identify positions
+    corner(nbt@spatial@final.prob)
+    
+    #calculate centroids for each cell (we do not need this step because here 1bin=1cell)
+    nbt.centroids=GetCentroids(nbt); colnames(nbt.centroids)=c("bin.tier","bin.dv")
+    corner(nbt.centroids)
+    
+    mapping='refined'
+    resultsAfterRefine=accuracyTest(posMat,geometry,nbt,convTable,raw.data,numberGenes,mapping,varMethod)
+    print(resultsAfterRefine)
+  }
 }
-
-#remove 3040
-
-#write accuracy test
-
-
