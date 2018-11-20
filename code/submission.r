beeee6de76a50f3bc93730797c6d694782bfbe1f
Marouen <marouen.b.guebila@gmail.com>
2018-11-20 19:54:48 +0100

    added submission file

---
+++ code/submission.r
@@ -0,0 +1,101 @@
+
+library(synapser)
+
+
+#Submission 1 
+mySubmission <- File('../submissions/20/20genes.csv', parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614032", mySub, name="submission1") 
+
+mySubmission <- File("../submissions/24/40genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614031", mySub, name="submission1") 
+
+mySubmission <- File("../submissions/28/60genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614030", mySub, name="submission1") 
+
+#Submission 2
+mySubmission <- File("../submissions/22/20genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614032", mySub, name="submission2")
+#-> replaced [1240,B] 3.40 to 3039
+
+mySubmission <- File("../submissions/26/40genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614031", mySub, name="submission2") 
+
+mySubmission <- File("../submissions/30/60genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614030", mySub, name="submission2") 
+
+#submission 3
+mySubmission <- File("../submissions/32/20genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614032", mySub, name="submission3") 
+
+mySubmission <- File("../submissions/36/40genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614031", mySub, name="submission3") 
+
+mySubmission <- File("../submissions/40/60genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614030", mySub, name="submission3") 
+
+#submission 4
+mySubmission <- File("../submissions/35/20genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614032", mySub, name="submission4") 
+
+mySubmission <- File("../submissions/39/40genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614031", mySub, name="submission4") 
+
+mySubmission <- File("../submissions/43/60genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614030", mySub, name="submission4") 
+
+#submission 5
+mySubmission <- File("../submissions/44/20genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614032", mySub, name="submission5") 
+
+mySubmission <- File("../submissions/48/40genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614031", mySub, name="submission5") 
+
+mySubmission <- File("../submissions/52/60genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614030", mySub, name="submission5") 
+
+#Final submission 1-2-1
+
+mySubmission <- File("../submissions/28/60genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614030", mySub, name="final_submission") 
+
+mySubmission <- File("../submissions/26/40genes.csv", parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614031", mySub, name="final_submission") 
+
+mySubmission <- File('../submissions/20/20genes.csv', parentId="syn17022610")
+mySub <- synStore(mySubmission)
+#The evaluationId HAS to be a string here or there will be an error
+submission <- synSubmit("9614032", mySub, name="final_submission") 
